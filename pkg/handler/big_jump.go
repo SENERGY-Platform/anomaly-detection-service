09967d9d31c5de9895b46f1d1c5ef7f9c6073ce5
c-schrade <schrade.christoph@gmail.com>
2025-03-14 11:30:08 +0100

    fix register names for big jump anom

--- pkg/handler/big_jump.go
+++ pkg/handler/big_jump.go
@@ -20,13 +20,13 @@ import "log"
 
 func init() {
 	/* Get Electricity Consumption, Electricity-->Total Subaspect, kWh */
-	Registry.Register("jump_back_anom_electricity_consumption_total_kwh", "urn:infai:ses:measuring-function:57dfd369-92db-462c-aca4-a767b52c972e", "urn:infai:ses:aspect:fdc999eb-d366-44e8-9d24-bfd48d5fece1", "urn:infai:ses:characteristic:3febed55-ba9b-43dc-8709-9c73bae3716e", 3, BigJumpHandler{})
+	Registry.Register("big_jump_anom_electricity_consumption_total_kwh", "urn:infai:ses:measuring-function:57dfd369-92db-462c-aca4-a767b52c972e", "urn:infai:ses:aspect:fdc999eb-d366-44e8-9d24-bfd48d5fece1", "urn:infai:ses:characteristic:3febed55-ba9b-43dc-8709-9c73bae3716e", 3, BigJumpHandler{})
 
 	/* Get Volume, Water, Liter */
-	Registry.Register("jump_back_anom_volume_water_liter", "urn:infai:ses:measuring-function:cfa56e75-8e8f-4f0d-a3fa-ed2758422b2a", "urn:infai:ses:aspect:b8b3b549-3b01-4604-a727-20aa528c21c9", "urn:infai:ses:characteristic:aeb260f8-5fe5-4989-9e66-3c0a4ff273c4", 3, BigJumpHandler{})
+	Registry.Register("big_jump_anom_volume_water_liter", "urn:infai:ses:measuring-function:cfa56e75-8e8f-4f0d-a3fa-ed2758422b2a", "urn:infai:ses:aspect:b8b3b549-3b01-4604-a727-20aa528c21c9", "urn:infai:ses:characteristic:aeb260f8-5fe5-4989-9e66-3c0a4ff273c4", 3, BigJumpHandler{})
 
 	/* Get Gas Consumption, Gas, Liter*/
-	Registry.Register("jump_back_anom_consumption_gas_liter", "urn:infai:ses:measuring-function:4daa591f-ad97-4e57-8014-aa3f5e552c3b", "urn:infai:ses:aspect:7ea324c1-48e4-419a-a499-325d79dac09f", "urn:infai:ses:characteristic:aeb260f8-5fe5-4989-9e66-3c0a4ff273c4", 3, BigJumpHandler{})
+	Registry.Register("big_jump_anom_consumption_gas_liter", "urn:infai:ses:measuring-function:4daa591f-ad97-4e57-8014-aa3f5e552c3b", "urn:infai:ses:aspect:7ea324c1-48e4-419a-a499-325d79dac09f", "urn:infai:ses:characteristic:aeb260f8-5fe5-4989-9e66-3c0a4ff273c4", 3, BigJumpHandler{})
 }
 
 type BigJumpHandler struct{}
