f7152632b1536d743a0efd44a2efa0a38a0715a2
c-schrade <schrade.christoph@gmail.com>
2025-04-02 12:13:07 +0200

    persist stddev, mean and # of date points for big jump anom

--- pkg/handler/big_jump.go
+++ pkg/handler/big_jump.go
@@ -23,35 +23,58 @@ import (
 
 func init() {
 	/* Get Electricity Consumption, Electricity-->Total Subaspect, kWh */
-	Registry.Register("big_jump_anom_electricity_consumption_total_kwh", "urn:infai:ses:measuring-function:57dfd369-92db-462c-aca4-a767b52c972e", "urn:infai:ses:aspect:fdc999eb-d366-44e8-9d24-bfd48d5fece1", "urn:infai:ses:characteristic:3febed55-ba9b-43dc-8709-9c73bae3716e", 2, &BigJumpHandler{})
+	Registry.Register("big_jump_anom_electricity_consumption_total_kwh", "urn:infai:ses:measuring-function:57dfd369-92db-462c-aca4-a767b52c972e", "urn:infai:ses:aspect:fdc999eb-d366-44e8-9d24-bfd48d5fece1", "urn:infai:ses:characteristic:3febed55-ba9b-43dc-8709-9c73bae3716e", 2, BigJumpHandler{})
 
 	/* Get Volume, Water, Liter */
-	Registry.Register("big_jump_anom_volume_water_liter", "urn:infai:ses:measuring-function:cfa56e75-8e8f-4f0d-a3fa-ed2758422b2a", "urn:infai:ses:aspect:b8b3b549-3b01-4604-a727-20aa528c21c9", "urn:infai:ses:characteristic:aeb260f8-5fe5-4989-9e66-3c0a4ff273c4", 2, &BigJumpHandler{})
+	Registry.Register("big_jump_anom_volume_water_liter", "urn:infai:ses:measuring-function:cfa56e75-8e8f-4f0d-a3fa-ed2758422b2a", "urn:infai:ses:aspect:b8b3b549-3b01-4604-a727-20aa528c21c9", "urn:infai:ses:characteristic:aeb260f8-5fe5-4989-9e66-3c0a4ff273c4", 2, BigJumpHandler{})
 
 	/* Get Gas Consumption, Gas, Liter*/
-	Registry.Register("big_jump_anom_consumption_gas_liter", "urn:infai:ses:measuring-function:4daa591f-ad97-4e57-8014-aa3f5e552c3b", "urn:infai:ses:aspect:7ea324c1-48e4-419a-a499-325d79dac09f", "urn:infai:ses:characteristic:aeb260f8-5fe5-4989-9e66-3c0a4ff273c4", 2, &BigJumpHandler{})
+	Registry.Register("big_jump_anom_consumption_gas_liter", "urn:infai:ses:measuring-function:4daa591f-ad97-4e57-8014-aa3f5e552c3b", "urn:infai:ses:aspect:7ea324c1-48e4-419a-a499-325d79dac09f", "urn:infai:ses:characteristic:aeb260f8-5fe5-4989-9e66-3c0a4ff273c4", 2, BigJumpHandler{})
 }
 
-type BigJumpHandler struct {
-	CurrentStddev float64 /* Std deviation of differences between consecutive meter values*/
-	CurrentMean   float64 /* Mean of differences between consecutive meter values*/
-	NumDatepoints int     /* Number of tracked differences up to now*/
-}
+type BigJumpHandler struct{}
 
-func (this *BigJumpHandler) Handle(context Context, values []interface{}) (anomaly bool, description string, err error) {
+func (this BigJumpHandler) Handle(context Context, values []interface{}) (anomaly bool, description string, err error) {
 	castValues, err := CastList[float64](values)
 	if err != nil {
 		return false, "", err
 	}
 	log.Println("Values:", castValues)
 
+	/* Std deviation of differences between consecutive meter values*/
+	var CurrentStddev float64
+	err = context.Store.Get(context.PrepareKey("big_jump", "stddev"), CurrentStddev)
+	if err != nil {
+		CurrentStddev = 0
+	}
+
+	/* Mean of differences between consecutive meter values*/
+	var CurrentMean float64
+	err = context.Store.Get(context.PrepareKey("big_jump", "mean"), CurrentMean)
+	if err != nil {
+		CurrentMean = 0
+	}
+
+	/* Number of tracked differences up to now*/
+	var NumDatepoints int
+	err = context.Store.Get(context.PrepareKey("big_jump", "num_datepoints"), NumDatepoints)
+	if err != nil {
+		NumDatepoints = 0
+	}
+
 	latestDifference := castValues[1] - castValues[0]
 
-	var bigJump bool = latestDifference > this.CurrentMean+5*this.CurrentStddev
+	var bigJump bool = latestDifference > CurrentMean+5*CurrentStddev
 
-	this.CurrentStddev = UpdateStddev(latestDifference, this.CurrentStddev, this.CurrentMean, this.NumDatepoints)
-	this.CurrentMean = UpdateMean(latestDifference, this.CurrentMean, this.NumDatepoints)
-	this.NumDatepoints = this.NumDatepoints + 1
+	CurrentStddev = UpdateStddev(latestDifference, CurrentStddev, CurrentMean, NumDatepoints)
+	context.Store.Set(context.PrepareKey("big_jump", "stddev"), CurrentStddev)
+
+	CurrentMean = UpdateMean(latestDifference, CurrentMean, NumDatepoints)
+	context.Store.Set(context.PrepareKey("big_jump", "mean"), CurrentMean)
+
+	NumDatepoints = NumDatepoints + 1
+	context.Store.Set(context.PrepareKey("big_jump", "num_datepoints"), NumDatepoints)
+
 	if bigJump {
 		log.Println("Meter reading had big jump.")
 		return true, "Meter reading had big jump.", nil
@@ -59,10 +82,12 @@ func (this *BigJumpHandler) Handle(context Context, values []interface{}) (anoma
 	return false, "", nil
 }
 
+/*Sample Update of standard deviation*/
 func UpdateStddev(latestValue float64, CurrentStddev float64, CurrentMean float64, NumDatepoints int) float64 {
 	return math.Sqrt(float64(NumDatepoints)/(float64(NumDatepoints+1))*math.Pow(CurrentStddev, 2) + float64(NumDatepoints)/math.Pow(float64(NumDatepoints)+1, 2)*math.Pow(latestValue-CurrentMean, 2))
 }
 
+/*Sample Update of mean*/
 func UpdateMean(latestValue float64, CurrentMean float64, NumDatepoints int) float64 {
 	return (float64(NumDatepoints)*CurrentMean + latestValue) / (float64(NumDatepoints) + 1)
 }
